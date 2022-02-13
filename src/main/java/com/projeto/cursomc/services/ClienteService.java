e2aa7b287ae43617e8c831c622eddf93b8e37f2a
Felipe Campos <felipecamposlv@gmail.com>
2022-02-13 19:08:22 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/projeto/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.projeto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.cursomc.domain.Cliente;
+import com.projeto.cursomc.repositories.ClienteRepository;
+import com.projeto.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
