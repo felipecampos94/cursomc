0080970fe35b52cf07617001f3a4c0b2180bc3ce
Felipe Campos <felipecamposlv@gmail.com>
2022-02-15 21:46:02 -0300

    Delete categoria

--- src/main/java/com/projeto/cursomc/services/CategoriaService.java
+++ src/main/java/com/projeto/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.projeto.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.projeto.cursomc.domain.Categoria;
 import com.projeto.cursomc.repositories.CategoriaRepository;
+import com.projeto.cursomc.services.exceptions.DataIntegrityException;
 import com.projeto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+
 	}
 
 }
