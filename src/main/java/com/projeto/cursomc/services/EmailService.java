8c9980584f320f4c2bb11bcc7c4d1ffd20129575
Felipe Campos <felipecamposlv@gmail.com>
2022-03-23 00:29:13 -0300

    Email HTML

--- src/main/java/com/projeto/cursomc/services/EmailService.java
+++ src/main/java/com/projeto/cursomc/services/EmailService.java
@@ -1,13 +1,19 @@
 package com.projeto.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.projeto.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 
 }
