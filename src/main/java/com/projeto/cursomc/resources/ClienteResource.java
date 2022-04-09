30baefe46dc2d2cbc8aba7fc846ec9feb8e124c9
felipecampos94 <felipecamposlv@gmail.com>
2022-04-09 17:50:52 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/projeto/cursomc/resources/ClienteResource.java
+++ src/main/java/com/projeto/cursomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.projeto.cursomc.domain.Categoria;
 import com.projeto.cursomc.domain.Cliente;
-import com.projeto.cursomc.dto.CategoriaDTO;
 import com.projeto.cursomc.dto.ClienteDTO;
 import com.projeto.cursomc.dto.ClienteNewDTO;
 import com.projeto.cursomc.services.ClienteService;
@@ -37,6 +36,7 @@ public class ClienteResource {
 		return ResponseEntity.ok(obj);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
@@ -62,12 +62,14 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
 	public ResponseEntity<Page<ClienteDTO>> findPage(
 		@RequestParam(value = "page", defaultValue = "0") Integer page, 
